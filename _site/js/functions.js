ec69f65ebff7f7cb22b1fa49f8a85e6912719344
Gus <gustavo.schiavon@gmail.com>
2020-12-15 12:25:28 -0300

    clean cache

--- _site/js/functions.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger')
-const nav = document.getElementById('nav-menu')
-
-burger.addEventListener('click', function(){
-  this.classList.toggle('open')
-  nav.classList.toggle('mobile-nav')
-})
